255dbcf6ca1d44ea757a5c17f7c75b8548216a46
Alex Ergasti <ergasti.alex@gmail.com>
2020-04-15 21:47:10 +0200

    New Feature FloatingButton
    Change how the floating button work and the option

--- src/FloatingButton/FloatingButton.js
+++ src/FloatingButton/FloatingButton.js
@@ -1,18 +1,21 @@
-import React, {useState} from 'react';
-import {View, StyleSheet, TouchableOpacity} from 'react-native';
-const FloatingButton = props => {
+import React, { useState } from "react";
+import { View, StyleSheet, TouchableOpacity } from "react-native";
+
+const FloatingButton = (props) => {
   const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <View style={styles.mainView}>
+    <View style={getMainViewStyle(props.position)}>
       <TouchableOpacity
-        style={{...styles.view, ...props.style}}
+        style={{ ...styles.view, backgroundColor: props.backgroundColor }}
         onPress={() => {
           if (props.hasChildren === true) {
             setIsOpen(!isOpen);
           } else {
             props.onPress();
           }
-        }}>
+        }}
+      >
         {props.icon}
       </TouchableOpacity>
       {/* To display the button */}
@@ -21,20 +24,34 @@ const FloatingButton = props => {
   );
 };
 
+FloatingButton.defaultProps = {
+  position: "flex-end",
+  backgroundColor: "red",
+  hasChildren: true,
+};
+
+const getMainViewStyle = (position) => {
+  alert(position)
+  return {
+    position: "absolute",
+    left: 0,
+    right: 0,
+    bottom:0,
+    flexDirection: "column-reverse",
+    alignItems:position,
+  };
+};
+
 const styles = StyleSheet.create({
-  mainView: {
-    position: 'absolute',
-    right: '8%',
-    bottom: 30,
-    flexDirection: 'column-reverse',
-    alignItems: 'flex-end',
-  },
+  //Container of the button and the child
   view: {
     width: 55,
     height: 55,
     borderRadius: 30,
-    justifyContent: 'center',
-    alignItems: 'center',
+
+    //For the icon
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
 
